4cb1c472142807da31f2175840a179e51193dbc2
Louys-GA <louysgenius@gmail.com>
2022-07-21 08:53:06 -0400

    Backend para listar productos por categoria

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,6 +4,25 @@ const asyncForEach = require('../utils/async_foreach');
 
 module.exports = {
 
+
+
+    async findByCategory(req, res, next) {
+        try {
+            
+            const id_category = req.params.id_category; //CLIENTE
+            const data = await Product.findByCategory(id_category);
+            return res.status(201).json(data);
+
+        } catch (error) {
+            console.log(`Error: ${error}`);
+                return res.status(501).json({
+                    message: `Error al listar los productos por categoria ${error}`,
+                    success: false,
+                    error: error
+                });
+        }
+    },
+
     async create(req, res, next) {
 
         let product = JSON.parse(req.body.product);
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,4 +5,6 @@ module.exports = (app, upload) => {
 
     app.post('/api/products/create', passport.authenticate('jwt', {session: false}), upload.array('image', 3), ProductsController.create);
 
+    app.get('/api/products/findByCategory/:id_category', passport.authenticate('jwt', {session: false}), ProductsController.findByCategory);
+
 }
