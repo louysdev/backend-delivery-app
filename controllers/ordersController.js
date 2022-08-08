387c7cfb0419c42e7207a943dab7b6956c9b91b2
Louys <louysgenius@gmail.com>
2022-08-08 03:05:04 -0400

    Actualizar el estado de la orden a entregado

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -75,6 +75,28 @@ async updateToOnTheWay(req, res, next){
     }
 },
 
+async updateToDelivered(req, res, next){
+    try {
+        
+        let order = req.body;
+        order.status = 'ENTREGADO';
+        const data = await Order.update(order);
+
+        return res.status(201).json({
+            success: true,
+            message: 'La orden se actualizo correctamente',
+        });
+
+    } catch (error) {
+        console.log(`Error ${error}`);
+        return res.status(501).json({
+            success: false,
+            message: 'Hubo un error al actualizar la orden',
+            error: error
+        });
+    }
+},
+
 async findByStatus(req, res, next) {
     try {
         const status = req.params.status;
